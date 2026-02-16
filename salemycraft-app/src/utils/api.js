db1d4abb0c741a01f04431553e76ab6ecc862a1f
salemycraftapp <salemycraft.app@gmail.com>
2026-02-16 22:00:09 +0530

    Update api.js

--- salemycraft-app/src/utils/api.js
+++ salemycraft-app/src/utils/api.js
@@ -2,6 +2,6 @@ export const LOCAL_URL = "http://localhost:3001";
 export const PROD_URL = "https://www.api.salemycraft.com";
 
 export const API = {
-  signup: `${LOCAL_URL}/api/v1/users/signup`,
-  login: `${LOCAL_URL}/api/v1/users/login`,
+  signup: `${PROD_URL}/api/v1/users/signup`,
+  login: `${PROD_URL}/api/v1/users/login`,
 };
